fbd4a077e305a2e8b66c00dbc22e362b2db3f8f2
Shamim Enamul <shamim.enamul23@gmail.com>
2020-03-03 18:14:05 +0600

    last update

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use('/admin',addminRouter);
 app.use(shopRouter);
 app.use('/',(req, res, next)=>
 {
-    res.status(404).send('<h1> Page Not Found');
+    res.status(404).send('<h1> Page Not Found</h1>');
 });
 
 app.listen(3000);
